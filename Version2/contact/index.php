b89b0df01887912db4032a0f5420ea3eaa4ce8cc
Bubbs <mookyd@gmail.com>
2018-08-08 13:27:45 -0700

    Adding DB Driven contact page

--- Version2/contact/index.php
+++ Version2/contact/index.php
@@ -1,4 +1,4 @@
-<!DOCTYPE HTML>  
+<!DOCTYPE HTML>
 <html>
 <head>
 <title>MyMooky.com</title>
@@ -24,6 +24,7 @@
   <a href="https://www.youtube.com/user/mookydesai/videos" target=new>YouTube</a>
   <a href="https://www.instagram.com/mymooky/" target=new>Instagram</a>
   <a href="http://mymooky.slack.com" target=new>Slack</a>
+  <a href="http://www.mymooky.com/contact" target=new>Contact</a>
 </div>
 
 
@@ -33,154 +34,49 @@
 .error {color: #FF0000;}
 </style>
 </head>
-<body>  
-
-<?php
-// define variables and set to empty values
-$nameErr = $emailErr = $genderErr = $websiteErr = "";
-$name = $email = $gender = $comment = $website = "";
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  if (empty($_POST["name"])) {
-    $nameErr = "Name is required";
-  } else {
-    $name = test_input($_POST["name"]);
-    // check if name only contains letters and whitespace
-    if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
-      $nameErr = "Only letters and white space allowed"; 
-    }
-  }
-  
-  if (empty($_POST["email"])) {
-    $emailErr = "Email is required";
-  } else {
-    $email = test_input($_POST["email"]);
-    // check if e-mail address is well-formed
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-      $emailErr = "Invalid email format"; 
-    }
-  }
-    
-  if (empty($_POST["website"])) {
-    $website = "";
-  } else {
-    $website = test_input($_POST["website"]);
-    // check if URL address syntax is valid (this regular expression also allows dashes in the URL)
-    if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website)) {
-      $websiteErr = "Invalid URL"; 
-    }
-  }
-
-  if (empty($_POST["comment"])) {
-    $comment = "";
-  } else {
-    $comment = test_input($_POST["comment"]);
-  }
-
-  if (empty($_POST["gender"])) {
-    $genderErr = "Gender is required";
-  } else {
-    $gender = test_input($_POST["gender"]);
-  }
-}
-
-function test_input($data) {
-  $data = trim($data);
-  $data = stripslashes($data);
-  $data = htmlspecialchars($data);
-  return $data;
-}
-?>
-<br>
-<br>
-<center>
-<img src="http://assets.mymooky.com/logo.jpg"></img>
-</center>
-
-<h2>Contact Me at MyMooky.com</h2>
-<p><span class="error">* required field</span></p>
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
-  Name: <input type="text" name="name" value="<?php echo $name;?>">
-  <span class="error">* <?php echo $nameErr;?></span>
-  <br><br>
-  E-mail: <input type="text" name="email" value="<?php echo $email;?>">
-  <span class="error">* <?php echo $emailErr;?></span>
-  <br><br>
-  Website: <input type="text" name="website" value="<?php echo $website;?>">
-  <span class="error"><?php echo $websiteErr;?></span>
-  <br><br>
-  Gender:
-  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="female") echo "checked";?> value="female">Female
-  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="male") echo "checked";?> value="male">Male
-  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="other") echo "checked";?> value="other">Other  
-  <span class="error">* <?php echo $genderErr;?></span>
-  <br><br>
-  Comment: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
-  <br><br>
-  <input type="submit" name="submit" value="Submit">  
-</form>
-
-<?php
-echo "<h2>Your Input:</h2>";
-echo $name;
-echo "<br>";
-echo $email;
-echo "<br>";
-echo $website;
-echo "<br>";
-echo $comment;
-echo "<br>";
-echo $gender;
-?>
-
-<?php
-$to = "mookyd@gmail.com";
-$subject = "Email From MyMooky.com";
-
-$message = "
-<html>
-<head>
-<title>Thank you!</title>
-</head>
 <body>
-<p>This email contains HTML Tags!</p>
-<table>
-<tr>
-<th>Name</th>
-<td>$name</td>
-</tr>
-<tr>
-<th>Email</th>
-<td>$email</td>
-</tr>
-<tr>
-<th>Website</th>
-<td>$website</td>
-</tr>
-<tr>
-<th>Gender</th>
-<td>$gender</td>
-</tr>
-<tr>
-<th>Comments</th>
-<td>$comment</td>
-</tr>
-</table>
-</body>
-</html>
-";
+<br>
+<br>
+<br>
+<form action="insert.php" method="post">
 
-// Always set content-type when sending HTML email
-$headers = "MIME-Version: 1.0" . "\r\n";
-$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+    <p>
+        <label for="CustomerName">Customer Name:</label>
+        <input type="text" name="customername" id="CustomerName">
+    </p>
 
-// More headers
-$headers .= 'From: <mooky@mymooky.com>' . "\r\n";
+    <p>
+        <label for="FirstName">First Name:</label>
+        <input type="text" name="firstname" id="FirstName">
+    </p>
+    <p>
+        <label for="LastName">Last Name:</label>
+        <input type="text" name="lastname" id="LastName">
+    </p>
 
-mail($to,$subject,$message,$headers);
-?>
+    <p>
+        <label for="Address">Address:</label>
+        <input type="text" name="address" id="Address">
+    </p>
+    <p>
+        <label for="City">City:</label>
+        <input type="text" name="city" id="City">
+    </p>
 
+    <p>
+        <label for="PostalCode">Zip Code:</label>
+        <input type="text" name="postalcode" id="PostalCode">
+    </p>
+    <p>
+        <label for="Country">Country:</label>
+        <input type="text" name="country" id="Country">
+    </p>
 
-
+    <p>
+        <label for="emailAddress">Email Address:</label>
+        <input type="text" name="email" id="emailAddress">
+    </p>
+    <input type="submit" value="Submit">
+</form>
 </body>
 </html>
